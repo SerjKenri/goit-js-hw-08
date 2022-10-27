e624e5a7f4cd5b5dda2649c1ff65479d6badcfc2
Sergey Kuzin <105937658+SerjKenri@users.noreply.github.com>
2022-10-27 18:43:11 +0300

    Finish homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate',  throttle( e => {
+    localStorage.setItem(STORAGE_KEY, e.seconds);
+    }, 1000)
+    );
+
+player
+.setCurrentTime(localStorage.getItem(STORAGE_KEY))
+.catch(function (error) {
+    console.error(error);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('textarea[name="message"]'),
+    email: document.querySelector('input[name="email"]')
+};
+
+const STORAGE_KEY = 'feedback-form-state';
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInput, 500));
+
+function onFormSubmit (evt) {
+    evt.preventDefault();
+
+    evt.currentTarget.reset();
+
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onInput (evt) {
+    const value = {email: refs.email.value, message: refs.textarea.value};
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(value));
+}
+
+
+const load = key => {
+    try {
+        const serializedState = localStorage.getItem(key);
+        return serializedState === null ? undefined : JSON.parse(serializedState);
+    } catch (error) {
+        console.error('Get state error: ', error.message);
+    }
+}
+
+const savedData = load(STORAGE_KEY);
+
+if(savedData){
+    refs.email.value = savedData.email;
+    refs.textarea.value = savedData.message;
+}
